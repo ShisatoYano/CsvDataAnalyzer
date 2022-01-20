430fd8354e222cc8245eb17a73877d7afac2e635
Shisato <shisatoyano@gmail.com>
2022-01-20 23:06:55 +0900

    renamed variable

--- src/csv_data_analyzer/domain_model/statistics/maximum_value_collection.py
+++ src/csv_data_analyzer/domain_model/statistics/maximum_value_collection.py
@@ -12,10 +12,10 @@ from .maximum_value import MaximumValue
 class MaximumValueCollection:
     def __init__(self):
         self.collection = {}
-        self.maxv = MaximumValue()
+        self.max_value = MaximumValue()
 
     def add(self, data_name, array):
-        self.collection[data_name] = self.maxv.calculate(array)
+        self.collection[data_name] = self.max_value.calculate(array)
 
     def get(self, data_name):
         return self.collection[data_name]
