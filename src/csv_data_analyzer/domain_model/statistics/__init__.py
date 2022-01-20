d18378dc0329dd853ae5be70daae09c023ac1f76
Shisato <shisatoyano@gmail.com>
2022-01-20 23:10:46 +0900

    add MinimumValueCollection module

--- src/csv_data_analyzer/domain_model/statistics/__init__.py
+++ src/csv_data_analyzer/domain_model/statistics/__init__.py
@@ -2,3 +2,5 @@ from .maximum_value import MaximumValue
 from .minimum_value import MinimumValue
 from .mean_value import MeanValue
 from .std_dev_value import StdDevValue
+from .maximum_value_collection import MaximumValueCollection
+from .minimum_value_collection import MinimumValueCollection
---
+++ src/csv_data_analyzer/domain_model/statistics/minimum_value_collection.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+"""
+MinimumValueCollection class
+
+Collect minimum value of each data
+"""
+
+from .minimum_value import MinimumValue
+
+
+class MinimumValueCollection:
+    def __init__(self):
+        self.collection = {}
+        self.min_value = MinimumValue()
+
+    def add(self, data_name, array):
+        self.collection[data_name] = self.min_value.calculate(array)
+
+    def get(self, data_name):
+        return self.collection[data_name]
