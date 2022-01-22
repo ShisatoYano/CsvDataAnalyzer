072820e9fc7195b68c376206d1715a318599e82f
Shisato <shisatoyano@gmail.com>
2022-01-22 16:06:55 +0900

    renamed class

--- src/csv_data_analyzer/domain_model/statistics/__init__.py
+++ src/csv_data_analyzer/domain_model/statistics/__init__.py
@@ -3,6 +3,6 @@ from .minimum_value import MinimumValue
 from .mean_value import MeanValue
 from .std_dev_value import StdDevValue
 from .maximum_values import MaximumValues
-from .minimum_value_collection import MinimumValueCollection
+from .minimum_values import MinimumValues
 from .mean_values import MeanValues
-from .std_dev_value_collection import StdDevValueCollection
+from .std_dev_values import StdDevValues
