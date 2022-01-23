74c3b5f8dd8e96c7fb4a43da778011ee87c54cae
Shisato <shisatoyano@gmail.com>
2022-01-23 15:41:13 +0900

    renamed method

--- test/test_line_2d_equal.py
+++ test/test_line_2d_equal.py
@@ -13,7 +13,7 @@ sys.path.append("../")
 from src.csv_data_analyzer.domain_model.visualization.line_2d_equal import Line2DEqual
 
 
-def test_line_2d_plot():
+def test_line_2d_equal_plot():
     line_2d_equal = Line2DEqual()
     assert line_2d_equal.plot([1, 2, 3, 4, 5], [6, 7, 8, 9, 10], "red") is True
     assert line_2d_equal.plot([1, 2, 3, 4, 5], [6, 7, 8], "red") is False
