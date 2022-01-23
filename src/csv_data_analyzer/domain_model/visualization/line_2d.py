205c33302bf219272973dde62c9652e98a405f73
Shisato <shisatoyano@gmail.com>
2022-01-23 15:24:49 +0900

    renamed member variables

--- src/csv_data_analyzer/domain_model/visualization/line_2d.py
+++ src/csv_data_analyzer/domain_model/visualization/line_2d.py
@@ -11,13 +11,13 @@ import matplotlib.pyplot as plt
 
 class Line2D:
     def __init__(self):
-        self.fig, self.axes = plt.subplots()
+        self._fig, self._axes = plt.subplots()
 
     def plot(self, data_x, data_y, color):
         if not self._is_same_size(data_x, data_y): return False
-        self.axes.plot(data_x, data_y, c=color,
-                       linestyle="solid", linewidth=3)
-        self.axes.grid(True)
+        self._axes.plot(data_x, data_y, c=color,
+                        linestyle="solid", linewidth=3)
+        self._axes.grid(True)
         return True
 
     def _is_same_size(self, data_x, data_y):
