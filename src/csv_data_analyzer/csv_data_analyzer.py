94f568337520d45fcd20b8f83ae4999d87294e22
Shisato <shisatoyano@gmail.com>
2022-04-08 21:10:08 +0900

    commit

--- src/csv_data_analyzer/csv_data_analyzer.py
+++ src/csv_data_analyzer/csv_data_analyzer.py
@@ -26,47 +26,48 @@ def main():
     # get and analyze command line input
     args = parser.parse_args()
 
-    # select a file
-    csv_object = CsvFile(args.file)
+    # read selected file
 
-    # Read selected file
-    reader = FileReader(csv_object.file_path())
-
-    # Calculate statistics
-    stats = Statistics()
-    stats.calculate(reader.data_frame(), reader.data_name_list())
-
-    # Plot data graph
-    l2d = Line2D()
-    l2d.plot(reader.data_array("pointNum"),
-             reader.data_array("player1Score"),
-             "red")
-    l2de = Line2DEqual()
-    l2de.plot(reader.data_array("pointNum"),
-              reader.data_array("player1Score"),
-              "red")
-    sc2d = Scatter2D()
-    sc2d.plot(reader.data_array("pointNum"),
-              reader.data_array("player1Score"),
-              "blue", "*")
-    sc2de = Scatter2DEqual()
-    sc2de.plot(reader.data_array("pointNum"),
-               reader.data_array("player1Score"),
-               "blue", "*")
-    hm2d = Heatmap2D()
-    hm2d.plot(reader.data_array("pointNum"),
-              reader.data_array("player1Score"),
-              reader.data_array("pointNum"),
-              min(reader.data_array("pointNum")),
-              max(reader.data_array("pointNum")))
-    hm2de = Heatmap2DEqual()
-    hm2de.plot(reader.data_array("pointNum"),
-               reader.data_array("player1Score"),
-               reader.data_array("pointNum"),
-               min(reader.data_array("pointNum")),
-               max(reader.data_array("pointNum")))
-
-    plt.show()
+    # csv_object = CsvFile(args.file)
+    #
+    # # Read selected file
+    # reader = FileReader(csv_object.file_path())
+    #
+    # # Calculate statistics
+    # stats = Statistics()
+    # stats.calculate(reader.data_frame(), reader.data_name_list())
+    #
+    # # Plot data graph
+    # l2d = Line2D()
+    # l2d.plot(reader.data_array("pointNum"),
+    #          reader.data_array("player1Score"),
+    #          "red")
+    # l2de = Line2DEqual()
+    # l2de.plot(reader.data_array("pointNum"),
+    #           reader.data_array("player1Score"),
+    #           "red")
+    # sc2d = Scatter2D()
+    # sc2d.plot(reader.data_array("pointNum"),
+    #           reader.data_array("player1Score"),
+    #           "blue", "*")
+    # sc2de = Scatter2DEqual()
+    # sc2de.plot(reader.data_array("pointNum"),
+    #            reader.data_array("player1Score"),
+    #            "blue", "*")
+    # hm2d = Heatmap2D()
+    # hm2d.plot(reader.data_array("pointNum"),
+    #           reader.data_array("player1Score"),
+    #           reader.data_array("pointNum"),
+    #           min(reader.data_array("pointNum")),
+    #           max(reader.data_array("pointNum")))
+    # hm2de = Heatmap2DEqual()
+    # hm2de.plot(reader.data_array("pointNum"),
+    #            reader.data_array("player1Score"),
+    #            reader.data_array("pointNum"),
+    #            min(reader.data_array("pointNum")),
+    #            max(reader.data_array("pointNum")))
+    #
+    # plt.show()
 
 
 if __name__ == "__main__":
